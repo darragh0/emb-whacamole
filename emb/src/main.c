2311796ff09b0c55026c2d62b711a09325c670c9
Tom J <thomasjdjoyce@gmail.com>
2025-12-02 23:45:12 +0000

    whackamole lighting

--- emb/src/main.c
+++ emb/src/main.c
@@ -8,12 +8,49 @@
 #include "stdbool.h"
 #include "utils.h"
 
-static void printbin(uint8_t x) {
-    for (int i = 7; i >= 0; i--) {
-        putchar(((x >> i) & 1) + '0');
-        if (i == 4) putchar(' ');
+/* random num generator with simple xorshift so no stdlib/time is required */
+static uint32_t rng_state = 0xDEADBEEF;
+static uint32_t next_rand(void)
+{
+    uint32_t x = rng_state;
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
+    rng_state = x;
+    return x;
+}
+
+
+static void random_button_lightup(void)
+{
+    while (true) {
+        int target = (int)(next_rand() % BUTTON_COUNT);
+
+        uint8_t led_pattern = 0;
+        led_on(target, &led_pattern);
+        if (io_expander_write_leds(led_pattern) != E_SUCCESS) return;
+
+        printf("Whack-a-mole: light at %d\n", target);
+
+        /* poll until the target button is pressed */
+        while (true) {
+            uint8_t btn_state = 0;
+            if (io_expander_read_btns(&btn_state) != E_SUCCESS) return;
+
+            if (is_btn_pressed(target, btn_state)) {
+
+                io_expander_write_leds(0);
+                printf("Hit %d\n", target);
+                MS_SLEEP(120);
+                break; 
+            }
+
+            MS_SLEEP(30);
+        }
     }
 }
+
+
 
 int main(void) {
     printf("\n=== \x1b[96mButton-to-LED Mapper\x1b[0m ===\n\n");
@@ -25,32 +62,7 @@ int main(void) {
 
     make_leds_look_pretty_n_shi(250, 500);
 
-    int btns_pressed[8] = {false};
-    while (true) {
-        uint8_t btn_state = 0;
-        if (io_expander_read_btns(&btn_state) != E_SUCCESS) break;
-
-        uint8_t led_pattern = 0;
-        for (int i = 0; i < BUTTON_COUNT; i++) {
-            if (is_btn_pressed(i, btn_state)) {
-                led_on(i, &led_pattern);
-                if (!btns_pressed[i]) {
-                    btns_pressed[i] = true;
-                    printf("Button \x1b[96m%d\x1b[0m pressed\n", i);
-                    printf("\x1b[2m  Button state: ");
-                    printbin(btn_state);
-                    printf("\n  LED state:    ");
-                    printbin(led_pattern);
-                    printf("\x1b[0m\n\n");
-                }
-            } else {
-                btns_pressed[i] = false;
-            }
-        }
-        io_expander_write_leds(led_pattern);
-
-        MS_SLEEP(50);
-    }
-
+    
+    random_button_lightup();
     return 0;
 }
